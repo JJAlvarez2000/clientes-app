5d3997bcf8e5d1cd7914a2fdd1563bb1b2f37074
Julio <alvarezjjoel6@gmail.com>
2023-01-29 17:52:56 -0300

    agregado de features

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,15 @@
+import { Cliente } from './cliente';
+
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Andres', apellido: 'Guzman', email: 'aguzman@live.com', createAt: '2017-12-11'},
+    {id: 2, nombre: 'Mr. John', apellido: 'Doe', email: 'johndoe@mail.com', createAt: '2017-12-12'},
+    {id: 3, nombre: 'Jane', apellido: 'Doe', email: 'jane@live.com', createAt: '2017-12-13'},
+    {id: 4, nombre: 'Peter', apellido: 'Parker', email: 'peterparker@gmail.com', createAt: '2017-12-14'},
+    {id: 5, nombre: 'Clark', apellido: 'Kent', email: 'superman@live.com', createAt: '2017-12-15'},
+    {id: 6, nombre: 'Bruce', apellido: 'Wayne', email: 'batman@live.com', createAt: '2017-12-16'},
+    {id: 7, nombre: 'Tony', apellido: 'Stark', email: 'tonyiron@gmail.com', createAt: '2017-12-17'},
+    {id: 8, nombre: 'Steve', apellido: 'Rogers', email: 'cptain@mail.com', createAt: '2017-12-18'},
+    {id: 9, nombre: 'Natasha', apellido: 'Romanoff', email: 'catwoman@mail.com', createAt: '2017-12-19'},
+    {id: 10, nombre: 'Thor', apellido: 'Odinson', email: 'asgardking@mail.com', createAt: '2017-12-20'},
+];
